eae793e773354fa6055ddd465a7cc6ac639208ce
hyunsik won <shik0601@gmail.com>
2020-01-23 14:19:57 +0900

    closure - map 연습

--- 맵,필터,리듀스/MapFilterReduce.playground/Contents.swift
+++ 맵,필터,리듀스/MapFilterReduce.playground/Contents.swift
@@ -8,3 +8,25 @@ let arr = [2, 15, 100, 79 , 20]
 let filteredArr = arr.filter (){$0 >= 20}
 
 print(filteredArr)
+
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four",
+    5: "Five", 6: "Siz", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+
+let numbers = [16, 58, 510]
+
+let nubmersToString = numbers.map { (number) -> String in
+    var str = ""
+    var number = number
+    
+    while number > 0 {
+        str = digitNames[number % 10]! + str
+        number /= 10
+    }
+    
+    return str
+    
+}
+
+print(nubmersToString)
